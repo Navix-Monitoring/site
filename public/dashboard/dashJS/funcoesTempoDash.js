3ab37d027a605bf0406116f535a777690c69ddeb
guilhermef-roveri <guilherme.roveri@sptech.school>
2025-11-27 16:42:10 -0300

    Funcao de comparacao entre datas feita

--- public/dashboard/dashJS/funcoesTempoDash.js
+++ public/dashboard/dashJS/funcoesTempoDash.js
@@ -1,44 +1,59 @@
+function formatarData(date) {
+    return date.toISOString().split("T")[0];
+}
+
+function calcularSemana(dia) {
+    if (dia <= 7) return 1;
+    if (dia <= 15) return 2;
+    if (dia <= 22) return 3;
+    return 4;
+}
+    let chartAlertasSemana = null;
+    let chartHardware = null;
+    let chartComparativo = null;
+
 async function verificarDia() {
+
+    document.getElementById("select_hardware_comparacao").style = "display: none;"
+
+
     const hoje = new Date();
-    const dataBR = hoje.toLocaleDateString("pt-BR");
 
     const ontem = new Date(hoje);
     ontem.setDate(hoje.getDate() - 1);
-    const ontemBR = ontem.toLocaleDateString("pt-BR");
 
-    const ano = hoje.getFullYear();
-    const mes = hoje.getMonth() + 1;
+    const anteontem = new Date(hoje);
+    anteontem.setDate(hoje.getDate() - 2);
 
-    let semana;
-    let numeroDiaSemana = ontem.getDate();
+    const input1 = document.getElementById("date1");
+    const input2 = document.getElementById("date2");
 
-    if (numeroDiaSemana <= 7) semana = 1;
-    else if (numeroDiaSemana <= 15) semana = 2;
-    else if (numeroDiaSemana <= 22) semana = 3;
-    else semana = 4;
+    // Se o input estiver vazio deixo como ontem e anteontem
+    if (!input1.value) input1.value = formatarData(ontem);
+    if (!input2.value) input2.value = formatarData(anteontem);
 
-    //Fazer double fetch e comparar com 11 dia antes do de ontem.
-    const dataAnteontem = new Date();
-    dataAnteontem.setDate(dataAnteontem.getDate() - 2);
-    const diaAnteontem = dataAnteontem.getDate();
-    const diaAnteOntemBR = dataAnteontem.toLocaleDateString("pt-BR")
+    const data1 = new Date(input1.value + "T00:00:00");
+    const data2 = new Date(input2.value + "T00:00:00");
 
-    const semanaOntem =
-        numeroDiaSemana <= 7 ? 1 :
-            numeroDiaSemana <= 15 ? 2 :
-                numeroDiaSemana <= 22 ? 3 : 4;
+    const ano = data1.getFullYear();
+    const mes = data1.getMonth() + 1;
+    const numeroDiaSemana = data1.getDate();
+    const semanaOntem = calcularSemana(numeroDiaSemana);
 
-    const semanaAnteontem =
-        diaAnteontem <= 7 ? 1 :
-            diaAnteontem <= 15 ? 2 :
-                diaAnteontem <= 22 ? 3 : 4;
+    const ano2 = data2.getFullYear();
+    const mes2 = data2.getMonth() + 1;
+    const diaAnteontem = data2.getDate();
+    const semanaAnteontem = calcularSemana(diaAnteontem);
+
+    const ontemBR = data1.toLocaleDateString("pt-BR");
+    const diaAnteOntemBR = data2.toLocaleDateString("pt-BR");
 
     document.getElementById("select_ontem").innerHTML = `Data`;
-   // document.getElementById("date1").value = `${ontem.getFullYear()}-${ontem.getMonth()}-${ontem.get()}`}
+
     try {
         const [resposta, resposta2] = await Promise.all([
             fetch(`/dashboard/diario/${ano}/${mes}/${semanaOntem}/${numeroDiaSemana}`),
-            fetch(`/dashboard/diario/${ano}/${mes}/${semanaAnteontem}/${diaAnteontem}`),
+            fetch(`/dashboard/diario/${ano2}/${mes2}/${semanaAnteontem}/${diaAnteontem}`),
         ]);
 
         if (!resposta.ok) {
@@ -47,12 +62,10 @@ async function verificarDia() {
         }
 
         const json = await resposta.json();
-
         const json2 = await resposta2.json();
 
-        //Variáveis para o anteontem
+        //Dados de anteontem ou input 2
 
-        //Lotes dele
         const lote1_2 = json2.find(d => d.lote === 1);
         const lote2_2 = json2.find(d => d.lote === 2);
         const lote3_2 = json2.find(d => d.lote === 3);
@@ -63,7 +76,6 @@ async function verificarDia() {
         const somaCriticoAnteOntem = lote1_2.totalCritico + lote2_2.totalCritico + lote3_2.totalCritico +
             lote4_2.totalCritico + lote5_2.totalCritico + lote6_2.totalCritico;
 
-            // --- SOMATÓRIOS POR HARDWARE ---
         const somaCPU_2 =
             lote1_2.cpuCritico + lote2_2.cpuCritico + lote3_2.cpuCritico +
             lote4_2.cpuCritico + lote5_2.cpuCritico + lote6_2.cpuCritico;
@@ -81,7 +93,7 @@ async function verificarDia() {
             lote4_2.tempCritico + lote5_2.tempCritico + lote6_2.tempCritico;
 
 
-        //Variáveis para o dia de ontem
+        //Dados de ontem ou input 1
 
         const lote1 = json.find(d => d.lote === 1);
         const lote2 = json.find(d => d.lote === 2);
@@ -94,7 +106,6 @@ async function verificarDia() {
 
         const top6 = listaLotes.sort((a, b) => b.totalAvisos - a.totalAvisos);
 
-        // --- SOMATÓRIOS DE NÍVEIS DE ALERTA ---
         const somaBaixo =
             lote1.totalBaixo + lote2.totalBaixo + lote3.totalBaixo +
             lote4.totalBaixo + lote5.totalBaixo + lote6.totalBaixo;
@@ -111,12 +122,11 @@ async function verificarDia() {
             lote1.totalCritico + lote2.totalCritico + lote3.totalCritico +
             lote4.totalCritico + lote5.totalCritico + lote6.totalCritico;
 
-        const somaTodosAlertas =
-            somaBaixo + somaNeutro + somaAtencao + somaCritico;
+        const somaTodosAlertas = somaBaixo + somaNeutro + somaAtencao + somaCritico;
 
-        document.getElementById("tituloLote").innerHTML = `Modelo NAV-M100 - 06 lotes - ${somaTodosAlertas} alertas`
+        document.getElementById("tituloLote").innerHTML =
+            `Modelo NAV-M100 - 06 lotes - ${somaTodosAlertas} alertas`;
 
-        // --- SOMATÓRIOS POR HARDWARE ---
         const somaCPU =
             lote1.cpuCritico + lote2.cpuCritico + lote3.cpuCritico +
             lote4.cpuCritico + lote5.cpuCritico + lote6.cpuCritico;
@@ -133,41 +143,36 @@ async function verificarDia() {
             lote1.tempCritico + lote2.tempCritico + lote3.tempCritico +
             lote4.tempCritico + lote5.tempCritico + lote6.tempCritico;
 
-        // --- KPIs STATUS ---
+        //KPIs
+
         document.getElementById("porcentagemBaixo").innerHTML =
             `${Math.round((somaBaixo * 100) / somaTodosAlertas)}%`;
-        document.getElementById("totalAlertaBaixoKPI").innerHTML =
-            `${somaBaixo} alertas`;
+        document.getElementById("totalAlertaBaixoKPI").innerHTML = `${somaBaixo} alertas`;
 
         document.getElementById("porcentagemNeutro").innerHTML =
             `${Math.round((somaNeutro * 100) / somaTodosAlertas)}%`;
-        document.getElementById("totalAlertaNeutroKPI").innerHTML =
-            `${somaNeutro} alertas`;
+        document.getElementById("totalAlertaNeutroKPI").innerHTML = `${somaNeutro} alertas`;
 
         document.getElementById("porcentagemAtencao").innerHTML =
             `${Math.round((somaAtencao * 100) / somaTodosAlertas)}%`;
-        document.getElementById("totalAlertaAtencaoKPI").innerHTML =
-            `${somaAtencao} alertas`;
+        document.getElementById("totalAlertaAtencaoKPI").innerHTML = `${somaAtencao} alertas`;
 
         document.getElementById("porcentagemCritico").innerHTML =
             `${Math.round((somaCritico * 100) / somaTodosAlertas)}%`;
-        document.getElementById("totalAlertaCriticoKPI").innerHTML =
-            `${somaCritico} alertas`;
+        document.getElementById("totalAlertaCriticoKPI").innerHTML = `${somaCritico} alertas`;
 
-        // --- KPIs HARDWARE ---
+        //Kpi do hardware
         document.getElementById("criticoRAM").innerHTML =
-            `RAM ${Math.round((somaRAM * 100) / somaCritico).toFixed(0)}%`;
-
+            `RAM ${Math.round((somaRAM * 100) / somaCritico)}%`;
         document.getElementById("criticoCPU").innerHTML =
-            `CPU ${Math.round((somaCPU * 100) / somaCritico).toFixed(0)}%`;
-
+            `CPU ${Math.round((somaCPU * 100) / somaCritico)}%`;
         document.getElementById("criticoDISCO").innerHTML =
-            `DISCO ${Math.round((somaDisco * 100) / somaCritico).toFixed(0)}%`;
-
+            `DISCO ${Math.round((somaDisco * 100) / somaCritico)}%`;
         document.getElementById("criticoTEMP").innerHTML =
-            `TEMP ${Math.round((somaTemp * 100) / somaCritico).toFixed(0)}%`;
+            `TEMP ${Math.round((somaTemp * 100) / somaCritico)}%`;
 
-        // --- RANKING ---
+        //Ranking de lote top6
+
         for (let i = 0; i < top6.length; i++) {
             document.getElementById(`numero_lote_top${i + 1}`).innerHTML =
                 `${i + 1}. LOTE A00${top6[i].lote}`;
@@ -175,35 +180,13 @@ async function verificarDia() {
                 `${top6[i].totalCritico} alertas`;
         }
 
-        // Gráfico de barras com os alertas juntos por lote
+        // Gráfico com total de alertas por lote
 
         var alertasSemana = {
-            chart: {
-                type: "bar",
-                height: 350,
-                stacked: true,
-                toolbar: { show: false }
-            },
-            plotOptions: {
-                bar: {
-                    barHeight: "90%"
-                }
-            },
-
-            title: {
-                text: `ALERTAS CRÍTICOS GERADOS - ${ontemBR}`,
-                align: "center"
-            },
-
-            colors: [
-                "#0a1a2f",
-                "#102f57",
-                "#1c47a1",
-                "#3d73ff",
-                "#253f6e",
-                "#4f83d1"
-            ],
-
+            chart: { type: "bar", height: 350, stacked: true, toolbar: { show: false } },
+            plotOptions: { bar: { barHeight: "90%" } },
+            title: { text: `ALERTAS CRÍTICOS GERADOS - ${ontemBR}`, align: "center" },
+            colors: ["#0a1a2f", "#102f57", "#1c47a1", "#3d73ff", "#253f6e", "#4f83d1"],
             series: [
                 { name: "Lote A001", data: [lote1_2.totalCritico, lote1.totalCritico] },
                 { name: "Lote A002", data: [lote2_2.totalCritico, lote2.totalCritico] },
@@ -212,13 +195,19 @@ async function verificarDia() {
                 { name: "Lote A005", data: [lote5_2.totalCritico, lote5.totalCritico] },
                 { name: "Lote A006", data: [lote6_2.totalCritico, lote6.totalCritico] }
             ],
-
-            xaxis: { categories: [`${diaAnteOntemBR}`, `${ontemBR}`] }
+            xaxis: { categories: [diaAnteOntemBR, ontemBR] }
         };
 
-        new ApexCharts(document.querySelector("#alertasSemana"), alertasSemana).render();
+        if (chartAlertasSemana) chartAlertasSemana.destroy();
 
-        // Comparacao entre hardware
+        chartAlertasSemana = new ApexCharts(
+            document.querySelector("#alertasSemana"),
+            alertasSemana
+        );
+
+        chartAlertasSemana.render();
+        //Gráfico que compara o hardware
+
         var totalAlertas = {
             chart: { type: "bar", height: 300 },
             stroke: { curve: "smooth", width: 3 },
@@ -228,46 +217,45 @@ async function verificarDia() {
                 { name: "Disco", data: [`${somaDisco_2}`, `${somaDisco}`] },
                 { name: "Temperatura", data: [`${somaTemp_2}`, `${somaTemp}`] },
             ],
-             colors: [
-                "#0a1a2f",
-                "#102f57",
-                "#1c47a1",
-                "#3d73ff",
-            ],
-            xaxis: {
-                categories: [diaAnteOntemBR, ontemBR]
-            }
+            colors: ["#0a1a2f", "#102f57", "#1c47a1", "#3d73ff"],
+            xaxis: { categories: [diaAnteOntemBR, ontemBR] }
         };
 
-        new ApexCharts(document.querySelector("#tempAlertChart"), totalAlertas).render();
+        if (chartHardware) chartHardware.destroy();
 
+        chartHardware = new ApexCharts(
+            document.querySelector("#tempAlertChart"),
+            totalAlertas
+        );
 
-        // Comparativo de alertas do dia com outro
-
+        chartHardware.render();
+        // Gráfico comparativo de alertas totais
 
         document.getElementById("tituloAlertasTotais").innerHTML = `Comparativo - Alertas Totais`;
         document.getElementById("subtituloAlertasTotais").innerHTML = `${ontemBR} x dia Anterior`;
 
-
         var comparativoAlertas = {
             chart: { type: "bar", height: 330 },
             plotOptions: { bar: { horizontal: true, distributed: true } },
-
             colors: ["#3b82f6", "#0a1a2f"],
-
-            series: [
-                {
-                    name: "Alertas Críticos",
-                    data: [somaCriticoAnteOntem, somaCritico]
-                }
-            ],
-
-            xaxis: { categories: [`${diaAnteOntemBR}`, `${ontemBR}`] }
+            series: [{
+                name: "Alertas Críticos",
+                data: [somaCriticoAnteOntem, somaCritico]
+            }],
+            xaxis: { categories: [diaAnteOntemBR, ontemBR] }
         };
 
-        new ApexCharts(document.querySelector("#comparativoAlertas"), comparativoAlertas).render();
+        if (chartComparativo) chartComparativo.destroy();
 
+        chartComparativo = new ApexCharts(
+            document.querySelector("#comparativoAlertas"),
+            comparativoAlertas
+        );
+
+        chartComparativo.render();
     } catch (erro) {
         console.error("ERRO NO FETCH:", erro);
     }
 }
+document.getElementById("date1").addEventListener("change", verificarDia);
+document.getElementById("date2").addEventListener("change", verificarDia);
