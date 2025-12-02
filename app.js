609cf9a4d52ee98e3683eebc8dbf0d9176390493
Celina Benedito <celina.benedito@sptech.school>
2025-12-02 14:32:47 -0300

    Add: rota para processos

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ var empresaRouter = require("./src/routes/empresa");
 var dashboardRouter = require("./src/routes/dashboard");
 var cadastroVeiculos = require("./src/routes/cadastroVeiculos")
 var dashGRouter = require("./src/routes/dashG");
+var dashProcessos = require("./src/routes/dashProcessos")
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -34,6 +35,7 @@ app.use("/empresa", empresaRouter);
 app.use("/dashboard", dashboardRouter);
 app.use("/cadastroVeiculos", cadastroVeiculos);
 app.use("/dashboard", dashGRouter);
+app.use("/dashProcessos", dashProcessos);
 
 app.listen(PORTA_APP, function () {
     console.log(`                                                                                               
